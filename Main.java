b35bb7ed666dde108dfbc388a0f88f1e1c0635a3
JLME19 <115184005+JLME19@users.noreply.github.com>
2023-09-28 14:07:43 -0400

    comit

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main{
+    public static void main(String[] args){
+        
+        system
+        color= 
+        connectivity
+        model
+        camera
+        button()
+
+    }
+ 
+    
+
+
+
+
+
+
+}
