c4e3651892728be5d228ed07441008be9459d8f1
JLME19 <115184005+JLME19@users.noreply.github.com>
2023-09-29 02:26:25 +0000

    objet simple

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
+/** Pilote du programme */
 public class Main{
     public static void main(String[] args){
+        //
+        Phone phone1 = new Phone();
+       
+        phone1.setModel();
+        phone1.setColor();
+        phone1.setCamera ();
         
-        system
-        color= 
-        connectivity
-        model
-        camera
-        button()
+        System.out.println ("Model:"+phone1.model);
+        System.out.println ("Color:"+phone1.color);
+        System.out.println ("Camera:"+phone1.camera);
+
+       
 
     }
  
--- Phone.java
+++ Phone.java
@@ -1,7 +1,23 @@
 public class Phone {
 
+// attributs
+String model ="Samsung";
+String color="Black"
+Double camera= 4.0;
+
+// comportements
+
+public void setModel (){
+this.model=model;
+}
 
 
+public void setColor (String color){
+    this.color= color;
+}
 
+public void setCamera (double camera)  {
+this.camera = camera;
+}
 
 }
