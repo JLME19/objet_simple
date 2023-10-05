c85e072d060f95714f3564e1e52443965888ba63
JLME19 <115184005+JLME19@users.noreply.github.com>
2023-10-05 10:29:25 -0400

    Encapsulation

--- Main.java
+++ Main.java
@@ -1,34 +1,29 @@
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //
-        Phone phone1 = new Phone("Samsung","Noir",4.0);
-        Phone phone2 = new Phone ("Iphone", "Bleue",3.0);
-       
+        Phone phone1 = new Phone("Samsung", "Noir", 4.0);
+        phone1.setColor("Rouge");
+
+        Phone phone2 = new Phone("Iphone", "Bleue", 3.0);
+
         phone1.setModel("Samsung");
         phone1.setColor("Noir");
         phone1.setCamera(4);
-        
-       // System.out.println ("Model:"+phone1.getModel());
-      //  System.out.println ("Color:"+phone1.getColor());
-        //System.out.println ("Camera:"+phone1.getCamera());
+
+        System.out.println("Model:" + phone1.getModel());
+        System.out.println("Color:" + phone1.getColor());
+        System.out.println("Camera:" + phone1.getCamera());
 
         System.out.println("Phone1");
-        System.out.println (phone1);
-       
+        System.out.println(phone1);
+
         System.out.println("Phone2");
-        System.out.println (phone2);
+        System.out.println(phone2);
 
         System.out.println("Phone1 le même que Phone2 ?");
         System.out.println(phone1.equals(phone2));
 
     }
- 
-    
-
-
-
-
-
 
 }
--- Phone.java
+++ Phone.java
@@ -1,22 +1,17 @@
 public class Phone {
 
     // attributs
-    private String model ;
-    private String color ;
+    private String model;
+    private String color;
     private double camera;
 
- public Phone(String model,String color,double camera){
-   
-  this. model=model;
-  this.color=color;
-  this.camera=camera;
+    public Phone(String model, String color, double camera) {
 
-    
-    
-   }
-   
+        this.model = model;
+        this.color = color;
+        this.camera = camera;
 
-
+    }
 
     // comportements
     public String getModel() {
@@ -35,49 +30,41 @@ public class Phone {
         this.color = color;
     }
 
-public double getCamera(){
-    return camera;
-}
+    public double getCamera() {
+        return camera;
+    }
 
     public void setCamera(double camera) {
         this.camera = camera;
     }
 
-
     @Override
     public String toString() {
         String output = "";
-        output += "Model: " + this.model+ "\n" ;
+        output += "Model: " + this.model + "\n";
         output += "Color: " + this.color + "\n";
         output += "Camera: " + this.camera + "\n";
-        
-        return output; 
-    }
-    
-@Override
-   
-public  boolean equals (Object other) {
 
-    if (this== other ){
-        return true;
+        return output;
     }
-   
-    if (!(other instanceof Phone)){
-        return false;
-    }
-    Phone o = (Phone)other;
 
-    return (o.model).equals(this.model )&& (o.color).equals(this.color);
+    @Override
+
+    public boolean equals(Object other) {
+
+        if (this == other) {
+            return true;
+        }
+
+        if (!(other instanceof Phone)) {
+            return false;
+        }
+        Phone o = (Phone) other;
+
+        return (o.model).equals(this.model) 
+            && (o.color).equals(this.color)
+            && o.camera == this.camera;
+
+    }
 
 }
-
-
-
-
-
-
-}
-
-
-
-
